039ee752ddc56698f691d590001adfb862c8a4ae
jocelynszhu <jszhu@mit.edu>
2023-08-01 09:30:26 -0400

    Added command

--- pipeline/main.py
+++ pipeline/main.py
@@ -16,7 +16,10 @@ def tracking(vid_path, dimension, input_size):
     for img in load_one_CV_PIL(vid_path, dimension):
         full_image, net_image, pad = get_image_tensor(img, input_size[0])
         pred = edgetpu.forward(net_image)
-                
         bbox = edgetpu.process_predictions(pred[0], full_image, pad)
-                
         print(bbox)
+
+vid_path = "kittens.AVI"
+dim = 224
+
+tracking(vid_path, dim, input_size)
