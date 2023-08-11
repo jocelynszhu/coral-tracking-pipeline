2f3ebb72616c686353c461691bbdce32e38e8b3f
jocelynszhu <jszhu@mit.edu>
2023-08-11 09:16:49 -0400

    just tracking pipeline

--- pipeline/main.py
+++ pipeline/main.py
@@ -8,33 +8,28 @@ import pycoral.utils.edgetpu as etpu
 
 
 
-def tracking(video, dimension, input_size):
+def tracking(video, dimension, input_size, writer):
     print("began tracking")
     mot_tracker = Sort(max_age=1, 
                        min_hits=3,
                        iou_threshold=0.3)
 
-    tracklets = {}
     i = 0
     for img, img_pil in load_one_SK_PIL(video, dimension):
         print("loaded image ", i)
         try:
             _, net_image, _ = get_image_tensor(img_pil, input_size[0])
             dets = yolo.predict(net_image) #list of obj detections
-            track(i, dimension, dets, mot_tracker, tracklets)
+            track(img, dimension, dets, mot_tracker, writer)
             print("tracked image")
         except:
             pass
         i += 1
-    print(tracklets)
-    return tracklets
-    # writer.release()
+    writer.release()
 
 
-def init_pipeline(vid_path, out_path, interpreter_file, detector_file, dim):
+def init_pipeline(vid_path, out_path, detector_file, dim):
 
-    interpreter = etpu.make_interpreter(interpreter_file)
-    interpreter.allocate_tensors()
 
     vidcap     = cv2.VideoCapture(vid_path)
     fps  = int(vidcap.get(cv2.CAP_PROP_FPS))
@@ -52,7 +47,7 @@ def init_pipeline(vid_path, out_path, interpreter_file, detector_file, dim):
     yolo = EdgeTPUModel(detector_file, alg_info, conf_thresh=0.4, iou_thresh=0.3)
     
     print("initialized models and video")
-    return video, writer, interpreter, yolo
+    return video, writer, yolo
 
 
 if __name__ =="__main__":
@@ -63,15 +58,8 @@ if __name__ =="__main__":
     model_name = 'models/1964_3.tflite'
     interpreter_name = 'models/behavioral_model.tflite'
 
-    video, writer, interpreter, yolo = init_pipeline(vid_path, out_path, interpreter_name, model_name, dim)
+    video, writer, yolo = init_pipeline(vid_path, out_path, interpreter_name, model_name, dim)
 
-    tracklets_all = tracking(video, dim, yolo.get_image_size())
+    tracking(video, dim, yolo.get_image_size(), writer)
 
-    for id, tracklets in tracklets_all.items():
-        if len(tracklets_all[id]) >= 11:
-            tracklets_id = tracklets_all[id][0:11] #figure out how to deal with longer
-        else:
-            continue
-        tracklet_video = generate_tracklets(video, tracklets_id, id, dim)
-        id_behavior = behavior(tracklet_video, interpreter)
-        print(id_behavior)
+
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -62,7 +62,7 @@ def input_image_size(interpreter):
     return width, height, channels
 
 
-def track(frame_idx, dim, objs, mot_tracker, tracklets):
+def track(img, dim, objs, mot_tracker, writer):
     detections = []
     
     #format yolo detects for tracking
@@ -74,37 +74,16 @@ def track(frame_idx, dim, objs, mot_tracker, tracklets):
     detections = np.array(detections)
     trdata = []
     trdata = mot_tracker.update(detections)
-    
+    image =  cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     #determine tracklets
     for i in range(len(trdata.tolist())):
         coords = trdata.tolist()[i]
-        # x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+        x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
         name_idx = int(coords[4])
-        tracklet_info = [int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3]), frame_idx]
-        try:
-            tracklets[name_idx].append(tracklet_info)
-        except:
-            tracklets[name_idx] = [tracklet_info]
-    return tracklets
+        image = displayBoxes(image, [x1, y1, x2, y2], name_idx)
+    writer.write(image)
+    
 
-def generate_tracklets(viddata, tracklets, dim): 
-    tracklet_vid = []
-    for tracklet_info in tracklets:
-        mask = [int(tracklet_info[0]), int(tracklet_info[1]), int(tracklet_info[2]), int(tracklet_info[3])]
-        frame = viddata[tracklet_info[4]]
-        frame = mold_image(frame[:,:,:], dim)
-        masked_frame = rescale_img(mask, frame)
-        tracklet_vid.append(masked_frame)
-    return tracklet_vid
-
-def behavior(tracklets, interpreter):
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    interpreter.set_tensor(input_details[0]['index'], tracklets.detach().numpy().transpose(0,2,3,1))
-    interpreter.set_tensor(input_details[1]['index'], tracklets.detach().numpy().transpose(0,2,3,1)[...,:3])
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    return output_data
 
 def convert2bbox(x0, y0, x1, y1, dim):
     x = x0 + (x1 - x0) / 2
@@ -154,8 +133,6 @@ def rescale_img(mask, frame, mask_size=224):
     return padded_img
 
 def displayBoxes(frame, mask, id, animal_id=None, mask_id=None):
-    #frame is in np format 
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_thickness = 1
 
