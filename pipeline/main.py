4fda45174909ecd406fb94ac763627e194e514d3
jocelynszhu <jszhu@mit.edu>
2023-08-14 10:42:48 -0400

    changing when to load in interpreter

--- pipeline/main.py
+++ pipeline/main.py
@@ -30,7 +30,7 @@ def tracking(video, dimension, input_size, writer):
     return tracklets
 
 
-def init_pipeline(vid_path, out_path, detector_file, interpreter_file, dim):
+def init_pipeline(vid_path, out_path, detector_file, dim):
 
     vidcap     = cv2.VideoCapture(vid_path)
     fps  = int(vidcap.get(cv2.CAP_PROP_FPS))
@@ -50,12 +50,9 @@ def init_pipeline(vid_path, out_path, detector_file, interpreter_file, dim):
     yolo = EdgeTPUModel(detector_file, alg_info, conf_thresh=0.4, iou_thresh=0.3)
     print("loaded yolo")
 
-    interpreter = etpu.make_interpreter(interpreter_file)
-    interpreter.allocate_tensors()
-    print("loaded behavioral interpreter")
 
     print("initialized models and video")
-    return video, writer, yolo, interpreter
+    return video, writer, yolo
 
 
 if __name__ =="__main__":
@@ -66,10 +63,14 @@ if __name__ =="__main__":
     model_name = 'models/1964_3.tflite'
     interpreter_name = 'models/behavioral_model.tflite'
 
-    video, writer, yolo, interpreter = init_pipeline(vid_path, out_path, model_name, interpreter_name, dim)
+    video, writer, yolo= init_pipeline(vid_path, out_path, model_name, dim)
 
     tracklets_all = tracking(video, dim, yolo.get_image_size(), writer)
 
+    interpreter = etpu.make_interpreter(interpreter_name)
+    interpreter.allocate_tensors()
+
+    print("loaded behavioral interpreter")
     for id, tracklets in tracklets_all.items():
         if len(tracklets_all[id]) >= 11:
             tracklets_id = tracklets_all[id][0:11] #figure out how to deal with longer
